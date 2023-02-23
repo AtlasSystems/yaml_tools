646ff439061e365c13e9bfdcbb76bd94b2a9ec5d
Curtis Poston <cposton@atlas-sys.com>
2023-02-23 11:15:34 -0500

    Update variable names for comparison files

--- spec/compare_spec.rb
+++ spec/compare_spec.rb
@@ -1,24 +1,27 @@
 require 'yaml_tools'
 
 RSpec.describe YAMLTools::Comparer do
-  sourceFilePath = File.join(__dir__, 'support/compare_source.yml');
-  differenceFilePath = File.join(__dir__, 'support/compare_difference.yml');
+  originalFilePath = File.join(__dir__, 'support/compare_original.yml');
+  modifiedFilePath = File.join(__dir__, 'support/compare_modified.yml');
 
   describe '#compare' do
     it 'compares' do
       expected = File.read(File.join(__dir__, 'support/compare_expected.yml'));
 
       comparer = YAMLTools::Comparer.new
-      result = comparer.compare_files(sourceFilePath, differenceFilePath, false)
+      result = comparer.compare_files(originalFilePath, modifiedFilePath, false)
 
       expect(result).to eq(expected)
     end
 
     it 'compares including anchors' do
-        expected = File.read(File.join(__dir__, 'support/compare_include_anchors_expected.yml'));
+      expected = File.read(File.join(__dir__, 'support/compare_include_anchors_expected.yml'));
 
-        comparer = YAMLTools::Comparer.new
-        result = comparer.compare_files(sourceFilePath, differenceFilePath, true)
+      comparer = YAMLTools::Comparer.new
+      result = comparer.compare_files(originalFilePath, modifiedFilePath, true)
+
+      expect(result).to eq(expected)
+    end
 
         expect(result).to eq(expected)
       end
